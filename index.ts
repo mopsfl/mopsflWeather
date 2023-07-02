44b182dac1b77bf4ce82ed653835de88922009db
mopsfl <floda854@gmail.com>
2023-07-02 15:02:56 +0200

    updated

--- index.ts
+++ index.ts
@@ -1,17 +1,26 @@
-const DEV_MODE = true
+const DEV_MODE = false
 const HTTPS_SERVER = false
 
-import GetGeoLocation from "./modules/GeoLocation.js"
-import WeatherApi, { CityData } from "./modules/WeatherApi.js"
-import WeatherIcon from "./modules/WeatherIcon.js"
-import SearchCity from "./modules/SearchCity.js"
+import GetGeoLocation from "./modules/GeoLocation"
+import WeatherApi, { CityData } from "./modules/WeatherApi"
+import WeatherIcon from "./modules/WeatherIcon"
+import SearchCity from "./modules/SearchCity"
+import LoadingCircle from "./modules/LoadingCircle"
+import LocalStorage from "./modules/LocalStorage"
+import * as _ from "./index"
 
-window["modules"] = {
-    GetGeoLocation, WeatherApi, WeatherIcon, SearchCity
+window.modules = {
+    GetGeoLocation, WeatherApi, WeatherIcon, SearchCity, LoadingCircle, LocalStorage, _
 }
 
 const weatherApi = new WeatherApi()
 const weatherIcon = new WeatherIcon()
+const localStorage = new LocalStorage({
+    key: "_weatherdata_"
+})
+const loadingCircle = new LoadingCircle({
+    loading_circle: document.querySelector(".weather_data_loading")
+})
 const searchCity = new SearchCity({
     location_search_results: document.querySelector(".location_search_results"),
     location_search_input: document.querySelector(".location_search_input"),
@@ -24,7 +33,17 @@ GetGeoLocation(SetGeoLocation, ErrorCallback)
  * Functions
 */
 
-function SetGeoLocation(pos: GeolocationPosition) { window["coords"] = pos.coords }
+async function SetGeoLocation(pos: GeolocationPosition) {
+    window.coords = pos.coords
+    localStorage.Set("coords", JSON.stringify(window.coords))
+    
+    // Get Weather From Current GeoLocation
+    const results = await weatherApi.GetWeatherData({
+        lat: window.coords.latitude,
+        lon: window.coords.longitude,
+    })
+    window.currentWeather = results
+}
 function ErrorCallback(err: any) { throw err }
 
 /**
@@ -34,11 +53,6 @@ function ErrorCallback(err: any) { throw err }
 if(DEV_MODE) console.warn("App running on DEV_MODE")
 
 const location_search_input: HTMLInputElement = document.querySelector(".location_search_input");
-
-(async () => {
-    const currentWeather = await weatherApi.GetCurrentWeather(HTTPS_SERVER, DEV_MODE)
-    window["currentWeather"] = currentWeather
-})()
 
 location_search_input.addEventListener("input", async (e: any) => {
     if(!e.target.validity.valid && e.target.validity.valueMissing) return searchCity.ToggleResults(false)
@@ -51,13 +65,22 @@ location_search_input.addEventListener("input", async (e: any) => {
         searchCity.UpdateResults(search_results)
     } else searchCity.ToggleResults(false)
 
-    window["currentCitySearchResults"] = search_results
+    window.currentCitySearchResults = search_results
 })
 
-location_search_input.addEventListener("focus", () => { if(location_search_input.validity.valid && window["currentCitySearchResults"]?.length > 0) searchCity.ToggleResults(true) })
-location_search_input.addEventListener("focusout", () => { if(!location_search_input.validity.valid) searchCity.ToggleResults(false) })
+location_search_input.addEventListener("focus", () => { if(location_search_input.validity.valid && window.currentCitySearchResults?.length > 0) searchCity.ToggleResults(true) });
+location_search_input.addEventListener("focusout", () => { if(!location_search_input.validity.valid) searchCity.ToggleResults(false) });
 
-export default {
+export  {
     DEV_MODE,
     HTTPS_SERVER
+}
+
+declare global {
+    interface Window {
+        coords: GeolocationCoordinates,
+        modules: Object,
+        currentCitySearchResults: Array<Object>,
+        currentWeather: any
+    }
 }
