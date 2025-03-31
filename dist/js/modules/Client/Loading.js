f8ac54a8eda602d2dd42caff17b86d8c8d1f6c74
mopsfl <floda854@gmail.com>
2025-03-31 17:52:45 +0200

    remove dist modules

--- dist/js/modules/Client/Loading.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    Toggle(element, state) {
-        state == true ? element.removeClass("hide") : element.addClass("hide");
-    }
-};
